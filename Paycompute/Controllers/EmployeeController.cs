371dbcbd3d9ee2e3d9ea6d1a3f7983aa06273504
SaiyemBejoy <jsbejoy96@gmail.com>
2020-08-04 16:42:46 +0600

    EmployeeController: implemented Delete()

--- Paycompute/Controllers/EmployeeController.cs
+++ Paycompute/Controllers/EmployeeController.cs
@@ -201,5 +201,30 @@ namespace Paycompute.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            var model = new EmployeeDeleteViewModel()
+            {
+                Id = employee.Id,
+                FullName = employee.FullName
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EmployeeDeleteViewModel model)
+        {
+            await _employeeService.Delete(model.Id);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
